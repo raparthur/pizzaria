8893432d2d068858627a986b79ed6bf0a7c62725
Claudio Kruger <raparthur@gmail.com>
2022-09-04 23:41:12 -0300

    dump atualizado

--- dump.sql
+++ dump.sql
@@ -14,6 +14,13 @@ nome varchar(50),
 precoUnidArea decimal(4,2))
 engine InnoDB;
 
+create table sabor(
+id int primary key auto_increment,
+nome varchar(50),
+tipo_id int,
+foreign key (tipo_id) references tipo_pizza(id) on delete cascade)
+engine InnoDB;
+
 create table forma(
 id int primary key auto_increment,
 nome varchar(20))
@@ -29,11 +36,12 @@ create table pizza(
 id int primary key auto_increment,
 sabor varchar(50),
 forma_id int,
-tipo1_id int,
-tipo2_id int,
+sabor1_id int,
+sabor2_id int,
 pedido_id int,
 foreign key(forma_id) references forma(id) on delete cascade,
-foreign key (tipo1_id) references tipo_pizza(id) on delete cascade,
+foreign key (sabor1_id) references sabor(id) on delete cascade,
+foreign key (sabor2_id) references sabor(id) on delete cascade,
 foreign key(pedido_id) references pedido(id) on delete cascade)
 engine InnoDB;
 
